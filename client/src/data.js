03244859bd01dee991cce1c51422f19614d38235
IILMTII\Mathew <lovinmathew@hotmail.com>
2022-03-25 16:10:05 -0500

    implemented expressjs and heroku

---
+++ client/src/data.js
@@ -0,0 +1,37 @@
+const data = {
+    products: [{
+            id: '1',
+            name: 'H‑E‑B Select Ingredients Family Size Homestyle Lasagna 38 oz',
+            price: 8.22,
+            sku: 27676534,
+            image: 'https://images.heb.com/is/image/HEBGrocery/001812540-1?id=ydFR73&fmt=jpg&dpr=off&fit=constrain,1&wid=296&hei=296',
+            isTaxable: true
+        },
+        {
+            id: '2',
+            name: 'H‑E‑B Select Ingredients Riced Original Cauliflower 12 oz',
+            price: 2.35,
+            sku: 85765342,
+            image: 'https://images.heb.com/is/image/HEBGrocery/002233669-1?id=93eST2&fmt=jpg&dpr=off&fit=constrain,1&wid=296&hei=296',
+            isTaxable: false
+        },
+        {
+            id: '3',
+            name: 'H‑E‑B Organics Mixed Vegetables 16 oz',
+            price: 2.55,
+            sku: 41220040,
+            image: 'https://images.heb.com/is/image/HEBGrocery/001968486-1?id=eEfSX1&fmt=jpg&dpr=off&fit=constrain,1&wid=296&hei=296',
+            isTaxable: true
+        },
+        {
+            id: '4',
+            name: 'Tofurky Hickory Smoked Deli Slices 5.5 oz',
+            price: 2.97,
+            sku: 61411728,
+            image: 'https://images.heb.com/is/image/HEBGrocery/001369531-1?id=Ey7RN0&fmt=jpg&dpr=off&fit=constrain,1&wid=196&hei=196',
+            isTaxable: true
+        }
+    ]
+}
+
+export default data
