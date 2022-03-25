7b960d2622c7f6e2bbdd286c46071ac6db934b16
IILMTII\Mathew <lovinmathew@hotmail.com>
2022-03-25 16:38:32 -0500

    removed /build in .gitignore

---
+++ client/src/coupons.js
@@ -0,0 +1,13 @@
+const coupons = {
+    coupon: [{
+            cu_id: '1',
+            discount: 0.25
+        },
+        {
+            cu_id: '3',
+            discount: 0.1
+        }
+    ]
+}
+
+export default coupons
